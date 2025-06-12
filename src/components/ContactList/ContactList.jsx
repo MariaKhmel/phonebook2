ef84fb2ee6fae510d8328fe7c0c8b7e9d7b86401
Maria Khmel <mariakhmel8@gmail.com>
2025-06-12 23:38:36 +0300

    fx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,8 @@
-import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import { contacts } from "../../redux/selectors";
 import css from "./ContactList.module.css";
 
 const ContactList = ({ handleDeleteContact }) => {
-  const contacts = useSelector(contacts);
   return (
     <ul className={css.contactsList}>
       {contacts.map((contact) => (
